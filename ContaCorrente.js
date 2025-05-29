8b78ca90f8a98aff424093d35256107e65327beb
snaiderrr <filipekeven@hotmail.com>
2025-05-29 10:32:47 -0300

    Adicionando Herança na conta

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,52 +1,9 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class contaCorrente {
+export class contaCorrente extends Conta{
     static numerodeContas = 0;
-    
-    
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
-            this._cliente = novoValor;
-        }
-    }
-
-    get cliente(){
-        return this._cliente;
-    }
-
-
-
-
-    get saldo(){
-        return this._saldo;
-    }
-
-    constructor(agencia, cliente){
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
+    constructor(cliente, agencia){
+        super(0, cliente, agencia);
         contaCorrente.numerodeContas += 1;
-    }
-    
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-
-    }
-
-    depositar(valor) {
-        if(valor <= 100){
-            return;
-        }
-        this._saldo += valor;
-    }
-
-    transferir(valor, conta){
-        
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valor);
     }
 }
--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import { ContaPoupanca } from "./ContaPoupanca.js";
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
 
-const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+const contaCorrenteRicardo = new contaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
